790ff3ec534d8c34e40344b4d92f5b0d87209d5b
bibek  karki <bibekjang@gmail.com>
2017-08-14 23:03:06 -0700

    error 400

--- tableau/src/main/java/tableau/App.java
+++ tableau/src/main/java/tableau/App.java
@@ -2,6 +2,7 @@ package tableau;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -12,18 +13,14 @@ import java.io.Reader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLPermission;
+import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
 import java.util.UUID;
 
-import javax.xml.XMLConstants;
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
-import javax.xml.validation.Schema;
-import javax.xml.validation.SchemaFactory;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.mime.HttpMultipartMode;
@@ -33,33 +30,19 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.client.LaxRedirectStrategy;
 import org.json.JSONObject;
 import org.json.XML;
-import org.xml.sax.SAXException;
 
-import bindings.TsRequest;
-import bindings.TsResponse;
+
 
 public class App {
-	private static Marshaller s_jaxbMarshaller;
-	private static Unmarshaller s_jaxbUnmarshaller;
-
+	
 	public static void main(String[] args) throws IOException {
-		try {
-			JAXBContext jaxbContext = JAXBContext.newInstance(TsRequest.class, TsResponse.class);
-			SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-			Schema schema = schemaFactory.newSchema(new File("ts-api_2_4.xsd"));
-			s_jaxbMarshaller = jaxbContext.createMarshaller();
-			s_jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-			s_jaxbUnmarshaller.setSchema(schema);
-			s_jaxbMarshaller.setSchema(schema);
-		} catch (JAXBException | SAXException ex) {
-			throw new IllegalStateException("Failed to initialize the REST API");
-		}
 
 		String userName = "karki@unlv.nevada.edu";
 		String password = "Marina@82";
 		String signinFile = "C:\\Users\\Owner\\Google Drive\\Tableau\\Resources\\payloadSignin.xml";
 		// String projectId = "";
 		String workbookName = "World Indicators-En-US.twbx";
+		
 
 		// get file
 		File file = new File("C:\\Users\\Owner\\Google Drive\\Tableau\\Resources\\World Indicators-En-US.twbx");
@@ -69,33 +52,86 @@ public class App {
 		// return;
 		// call signin, get token
 		String token = signIn(userName, password, payloadSignin);
+		
+		publishWorkbook(userName, password,token, file);
 
 	}
 
-	private static String publishWorkbook(String token, File file, String requestPayload) {
+	private static void publishWorkbook(String userName, String password, String token, File file) throws IOException {
 		String url = "https://us-east-1.online.tableau.com/api/2.4/sites/69e5f477-52d3-4e92-9960-c138399ec120/workbooks";
-		Object credentialObject;
+		String projectId = "e61be408-6b51-451d-bd00-cb3bab4527a1";
+		
+		StringBuffer xmlString = new StringBuffer();
+		String boundaryString = UUID.randomUUID().toString();
+		String bString = boundaryString.replaceAll("-", "");
+	
+			String fileString = FileUtils.readFileToString(file);
 
-		URL serverURL = null;
+		
+		String body =
+			    "--" + bString + "\r\n" +
+			    "Content-Disposition: name=\"request_payload\"\r\n" +
+			    "Content-Type: text/xml\r\n\r\n" +
+			    "<tsRequest>\r\n" +
+			    "  <workbook name=\"World Indicators-En-US\" showTabs=\"false\" >\r\n" +
+			    "    <connectionCredentials name=\"" + userName + "\" password=\"" + password +"\" /> \r\n" +
+			    "    <project id=\"" + projectId + "\"/>\r\n" +
+			    "  </workbook>\r\n" +
+			    "</tsRequest>\r\n" +
+			    "\r\n--" + bString + "\r\n" +
+			    "Content-Disposition: name=\"tableau_workbook\"; filename=\"" + file.getName() + "\"\r\n" +
+			    "Content-Type: application/octet-stream\r\n\r\n" +
+			    fileString + "\r\n\r\n" +
+			    "--" + bString + "--";
+		
+		ByteBuffer bodyByteBuffer = Charset.forName("UTF-8").encode(body);
+		byte[] bodyBytes = new byte[bodyByteBuffer.remaining()];
+		bodyByteBuffer.get(bodyBytes);
+
+		int len= bodyBytes.length;
+		
+		
+		//HTTP URL Connection
+		HttpURLConnection conn = null;
+		
 		try {
-			serverURL = new URL(url);
+			URL urlPost = new URL(url);
+		
+			conn = (HttpURLConnection) urlPost.openConnection();
+			conn.setDoOutput(true);
+			conn.setRequestMethod("POST");
+			
+			conn.setDoOutput(true);
+			conn.setDoInput(true);
+			conn.setUseCaches(true);
+			conn.setRequestMethod("POST");
+
+			conn.setRequestProperty("X-Tableau-Auth", token);
+			conn.setRequestProperty("Content-Type", "multipart/mixed; boundary="+bString);
+			conn.setRequestProperty("Content-length", Integer.toString(len));
+			
+			DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
+			wr.write(bodyBytes);
+			wr.flush();
+			wr.close();
+			
+			System.out.println("Server Response Code :"+conn.getResponseCode());
+			
+			BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			String line;
+
+			while ((line = br.readLine()) != null) {
+				xmlString.append(line + "\n");
+			}
+
+			br.close();
+			conn.disconnect();
 		} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		HttpPost target = new HttpPost(serverURL.getHost());
-		RequestConfig rc = RequestConfig.custom().setCircularRedirectsAllowed(true).setRedirectsEnabled(true)
-				.setConnectionRequestTimeout(30000).setSocketTimeout(30000).setConnectTimeout(30000).build();
-		CloseableHttpClient httpclient = HttpClients.custom().setDefaultRequestConfig(rc)
-				.setRedirectStrategy(new LaxRedirectStrategy()).build();
-		HttpPost httpPost = new HttpPost(url);
-		String boundary = UUID.randomUUID().toString();
-		httpPost.setHeader("X-Tableu-Auth", token);
-		MultipartEntityBuilder mb = MultipartEntityBuilder.create();
-		mb.setBoundary(boundary);
-		mb.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
-		mb.setCharset(Charset.forName("UTF-8"));
-		return "";
+		
+		
 	}
 
 	private static String signIn(String userName, String password, String payload) throws IOException {
@@ -107,8 +143,11 @@ public class App {
 			URL url = new URL(urlSignIn);
 			connection = (HttpURLConnection) url.openConnection();
 			connection.setDoOutput(true);
+			connection.setDoInput(true);
+			connection.setUseCaches(true);
 			connection.setRequestMethod("POST");
 
+		
 			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream()));
 			writer.write(payload);
 			writer.flush();
